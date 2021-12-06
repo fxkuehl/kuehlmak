101afe2c38a694da36090074a483ebf317f61f98
Felix Kuehling <felix.kuehling@gmail.com>
2021-12-06 00:40:34 -0500

    Implement command line parsing
    Defined an "anneal" subcommand with parameters for text, layout, steps
    and layout shuffling. Layouts can be loaded from files or given by a
    hard-coded name. This allows loading files generated by previous
    annealing runs.
    
    Signed-off-by: Felix Kuehling <felix.kuehling@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,88 @@
 use kuehlmak::TextStats;
 use kuehlmak::{
-    layout_from_str, layout_to_str, layout_to_filename,
-    EvalModel, EvalScores, KuehlmakModel,
+    layout_from_str, EvalModel, EvalScores, KuehlmakModel,
     Anneal
 };
-//use std::str::FromStr;
+
+use clap::{clap_app, ArgMatches};
+
+use std::str::FromStr;
+use std::process;
 use std::io;
 use std::fs;
 
-//static TEST_STRING: &str = "Hello, world! Be well.";
+static QWERTY: &str =
+r#"q  w  e  r  t  y  u  i  o  p
+   a  s  d  f  g  h  j  k  l ;:
+   z  x  c  v  b  n  m ,< .> /?"#;
 
 static COLEMAK: &str =
 r#"q  w  f  p  g  j  l  u  y ;:
    a  r  s  t  d  h  n  e  i  o
-   z  x  c  v  b  k  m ,< .> /?
-Random garbage at the end of the string gets ignored.
-"#;
+   z  x  c  v  b  k  m ,< .> /?"#;
 
-fn main() {
-    //let stats = TextStats::from_str(TEST_STRING).unwrap();
-    let json = fs::read_to_string("benches/bench_text.json").unwrap();
-    let stats = serde_json::from_str::<TextStats>(&json).unwrap();
+static DVORAK: &str =
+r#"'" ,< .>  p  y  f  g  c  r  l
+    a  o  e  u  i  d  h  t  n  s
+   ;:  q  j  k  x  b  m  w  v  z"#;
+
+fn anneal_command(sub_m: &ArgMatches) {
+    let layout = match sub_m.value_of("layout") {
+        Some("QWERTY") => String::from(QWERTY),
+        Some("Colemak") => String::from(COLEMAK),
+        Some("Dvorak") => String::from(DVORAK),
+        Some(filename) => fs::read_to_string(filename).unwrap_or_else(|e| {
+            eprintln!("Failed to read layout file '{}': {}", filename, e);
+            process::exit(1)
+        }),
+        None => String::from(QWERTY),
+    };
+    let layout = layout_from_str(&layout).unwrap_or_else(|e| {
+        eprintln!("Failed to parse layout: {}", e);
+        process::exit(1)
+    });
+
+    // Won't panic because TEXT is mandatory
+    let text_filename = sub_m.value_of("TEXT").unwrap();
+    let text_contents = fs::read_to_string(text_filename).unwrap_or_else(|e| {
+        eprintln!("Failed to read TEXT file '{}': {}", text_filename, e);
+        process::exit(1)
+    });
+    let text = if text_filename.ends_with(".json") {
+        serde_json::from_str::<TextStats>(&text_contents).unwrap_or_else(|e| {
+            eprintln!("Failed to parse JSON file '{}': {}", text_filename, e);
+            process::exit(1)
+        })
+    } else {
+        // This shouldn't panic
+        TextStats::from_str(&text_contents).unwrap()
+    };
+    let mut alphabet: Vec<_> = layout.iter().flatten().copied().collect();
+    alphabet.sort();
+    let text = text.filter(|c| alphabet.binary_search(&c).is_ok());
+
+    let shuffle = !sub_m.is_present("noshuffle");
+    let steps: u64 = match sub_m.value_of("steps")
+                                .unwrap_or("10000").parse() {
+        Ok(num) => num,
+        Err(e) => {
+            eprintln!("Invalid value for --setps: {}\n{}", e, sub_m.usage());
+            process::exit(1)
+        }
+    };
 
     let kuehlmak_model = KuehlmakModel::new();
+    let mut anneal = Anneal::new(&kuehlmak_model, &text, layout, shuffle,
+                                 steps);
 
-    let layout = layout_from_str(COLEMAK).unwrap();
-    print!("{}", layout_to_str(&layout));
-    println!("{}", layout_to_filename(&layout));
-    let mut scores = kuehlmak_model.eval_layout(&layout, &stats, 1.0);
-
+    let mut scores = kuehlmak_model.eval_layout(&layout, &text, 1.0);
     let stdout = &mut io::stdout();
     scores.write(stdout).unwrap();
 
-    let mut anneal = Anneal::new(&kuehlmak_model, &stats, layout, true,
-                                 10000);
     loop {
         if let Some(s) = anneal.next() {
-            // VT100: cursor up 8 rows
-            print!("\x1b[8A");
+            // VT100: cursor up 9 rows
+            print!("\x1b[9A");
             // VT100 clear line (top row of the last keymap)
             print!("\x1b[2K");
             anneal.write_stats(stdout).unwrap();
@@ -50,4 +95,34 @@ fn main() {
     }
 
     scores.write_to_db("./db").unwrap();
+}
+
+fn main() {
+    let app_m = clap_app!(kuehlmak =>
+        (version: "0.1")
+        (author: "Felix Kuehling <felix.kuehling@gmail.com>")
+        (about: "Keyboard layout generator and analyzer")
+        (@subcommand anneal =>
+            (about: "Generate a layout with Simulated Annealing")
+            (version: "0.1")
+            (@arg layout: -l --layout +takes_value
+                "Initial layout name or filename [QWERTY]")
+            (@arg noshuffle: -n --("no-shuffle")
+                "Don't shuffle initial layout")
+            (@arg steps: -s --steps +takes_value
+                "Steps per annealing iteration [10000]")
+            (@arg TEXT: +required
+                "Text or JSON file to use as input")
+        )
+    ).get_matches();
+
+    match app_m.subcommand_name() {
+        Some("anneal") => anneal_command(app_m.subcommand_matches("anneal")
+                                              .unwrap()),
+        Some(unknown) => panic!("Unhandled subcommand: {}", unknown),
+        None => {
+            eprintln!("No subcommand given.\n{}", app_m.usage());
+            process::exit(1)
+        },
+    }
 }
