8c94206bae164b7af58f0442e446b345932770e7
Felix Kuehling <felix.kuehling@gmail.com>
2022-05-15 03:54:21 -0400

    scripts: Add helper script for wikipedia processing
    Uses wikiextractor to extract plain text from wikipedia database dumps
    and runs them through the textstats command.
    
    Signed-off-by: Felix Kuehling <felix.kuehling@gmail.com>

---
+++ scripts/process_wiki.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+script=$(realpath $0)
+kuehlmak=${script%/*}/../target/release/kuehlmak
+
+input=$1
+shift 1
+
+wikiextractor -o - $input | egrep -v '^</?doc' | $kuehlmak textstats "$@" -
